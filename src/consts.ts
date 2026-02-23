39d7f04f9cd183c669f878b640b51027f241d1ac
Xlqmu <3212929002@qq.com>
2026-02-23 20:38:50 +0800

    chore: remove unused site description/subtitle constants
    - delete SITE_DESCRIPTION and SITE_SUBTITLE from consts
    
    - remove related imports and BaseLayout description props from pages
    
    - make BaseLayout description optional and set RSS description explicitly

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Neomelt的个人博客';
-export const SITE_DESCRIPTION = '将缠绕在这世界上的话语，于追寻中寻觅中编织出来';
-export const SITE_SUBTITLE = '海底、骤雨、回忆之中';
+export const SITE_TITLE = "Neomelt's Blog";
