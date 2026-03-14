8cf01a1cb93c84d8b849556918c2784806c778e4
Xlqmu <3212929002@qq.com>
2026-03-14 15:55:44 +0800

    chore(pages): align fallback seo signals

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Neomelt's Blog";
+export const SITE_ORIGIN = "https://www.neomelt.cloud/";
